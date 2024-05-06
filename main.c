36e4fdc1ab7a12311d35e4351439a9b73934ee63
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2024-05-06 13:11:40 -0300

    colocando os requisitos

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <locale.h>
 
 int main(void) {
-    setlocale(LC_ALL, "Portuguese");
     printf("|===================================|\n");
     printf("| Bem vindo (a) ao Caixa Eletronico |\n");
     printf("|===================================|\n");
