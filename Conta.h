5c0342ee0119191a16b0d97898dac44c78bd1592
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2024-05-06 17:53:37 -0300

    voltando as origens

--- Conta.h
+++ Conta.h
@@ -1,6 +1,28 @@
 //
-// Created by KingP on 5/6/24.
+// Created by Fernando on 5/6/24.
 //
+#define MAX 100
+
+typedef struct Conta {
+    int numeroConta;
+    char nomeTitular[100];
+    double saldo;
+} conta;
+typedef conta *p_conta;
+
+extern p_conta v[MAX];
+
+/*
+void inicializa();
+void ler_dados(int i);
+void inicializa();
+void inicializa();
+void inicializa();
+ */
+
+
+
+
 
 #ifndef CONTA_H
 #define CONTA_H
--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int menu = 1;
+    printf("|===================================|\n");
+    printf("| Bem vindo (a) ao Caixa Eletronico |\n");
+    printf("|===================================|\n");
+    do {
+        printf("\t\tMENU\n");
+        printf("\t1.Criar uma conta\n");
+        printf("\t2.Depositar\n");
+        printf("\t3.Sacar\n");
+        printf("\t4.Visualizar saldo\n");
+        printf("\t5.Relatorio de transacoes\n");
+        printf("\t6.Sair\n");
+        printf("=================================\n");
+        printf("\nDigite uma das opcoes acima:");
+        scanf("%d",&menu);
+        switch (menu) {
+            case 1:
+                printf("\nEscolheu 1\n");
+            break;
+            case 2:
+                printf("\nEscolheu 2\n");
+            break;
+            case 3:
+                printf("\nEscolheu 3\n");
+            break;
+            case 4:
+                printf("\nEscolheu 4\n");
+            break;
+            case 5:
+                printf("\nEscolheu 5\n");
+            break;
+            case 6:
+                printf("\nEncerrando o programa.");
+            break;
+            default:
+                printf("\nVoce escolheu uma opcao invalida.\n");
+        }
+    }   while (menu != 6);
+
+
     return 0;
+
+
 }
